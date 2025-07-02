c126e6a21178a5613d3b843b90663bbb19ae4c61
Dinesh S <dinesh.s@aaludra.com>
2025-07-02 23:14:07 +0530

    port

--- server.js
+++ server.js
@@ -1,46 +1,54 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const db = require('./db');
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const db = require("./db");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static('public')); // for index.html and register.html
+app.use(express.static("public")); // for index.html and register.html
 
 // LOGIN API
-app.post('/api/login', (req, res) => {
+app.post("/api/login", (req, res) => {
   const { email, password } = req.body;
 
-  db.query('SELECT * FROM users WHERE email = ?', [email], (err, results) => {
-    if (err) return res.status(500).json({ error: 'Database error' });
+  db.query("SELECT * FROM users WHERE email = ?", [email], (err, results) => {
+    if (err) return res.status(500).json({ error: "Database error" });
 
     if (results.length === 0 || results[0].password !== password) {
-      return res.status(401).json({ error: 'Invalid credentials' });
+      return res.status(401).json({ error: "Invalid credentials" });
     }
 
-    res.json({ message: 'Login successful' });
+    res.json({ message: "Login successful" });
   });
 });
 
 // REGISTER API
-app.post('/api/register', (req, res) => {
+app.post("/api/register", (req, res) => {
   const { email, password } = req.body;
 
-  db.query('SELECT * FROM users WHERE email = ?', [email], (err, results) => {
-    if (err) return res.status(500).json({ error: 'Database error' });
+  db.query("SELECT * FROM users WHERE email = ?", [email], (err, results) => {
+    if (err) return res.status(500).json({ error: "Database error" });
 
     if (results.length > 0) {
-      return res.status(400).json({ error: 'Email already registered' });
+      return res.status(400).json({ error: "Email already registered" });
     }
 
-    db.query('INSERT INTO users (email, password) VALUES (?, ?)', [email, password], (err) => {
-      if (err) return res.status(500).json({ error: 'Error saving user' });
-      res.json({ message: 'Registered successfully' });
-    });
+    db.query(
+      "INSERT INTO users (email, password) VALUES (?, ?)",
+      [email, password],
+      (err) => {
+        if (err) return res.status(500).json({ error: "Error saving user" });
+        res.json({ message: "Registered successfully" });
+      }
+    );
   });
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0' ,() => console.log(`Server running on http://localhost:${PORT}`));
+// app.listen(PORT, '0.0.0.0' ,() => console.log(`Server running on http://localhost:${PORT}`));
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
+});
