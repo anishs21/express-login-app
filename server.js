91b451e01d4b4d75b777af9f240ac3ae16919f3b
Dinesh S <dinesh.s@aaludra.com>
2025-07-02 22:24:31 +0530

    port added for EC2

--- server.js
+++ server.js
@@ -43,4 +43,4 @@ app.post('/api/register', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0' ,() => console.log(`Server running on http://localhost:${PORT}`));
